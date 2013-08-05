6b8db62daea02ee9bdb4c5ab82aae4bbbda690c8
Joel Martin <github@martintribe.org>
2013-08-04 19:01:26 -0500

    Add package.json and index.js.

---
+++ index.js
@@ -0,0 +1,6 @@
+var base = require("./base");
+var local = require("./local");
+
+exports.RaftServerBase = base.RaftServerBase;
+exports.RaftServerLocal = local.RaftServerLocal;
+exports.RaftServerLocalDurable = local.RaftServerLocalDurable;
