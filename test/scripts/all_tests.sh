dd7c2ea350e0d1f5bd9c1ec0d57fee9f1475e336
Joel Martin <github@martintribe.org>
2015-12-07 02:36:42 -0600

    Update all_tests.sh and .gitignore
    - increase all_tests.sh cluster size to 35 and parameterize outdir
    - add *.bak to .gitignore

--- test/scripts/all_tests.sh
+++ test/scripts/all_tests.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+outdir=test/data_et1000
+
+mkdir -p ${outdir}
+
 # chat propagate 1 message:
-time for x in $(seq 1 7); do for nodes in $(seq 1 2 31); do time node test/wait_chat_propagate.js 192.168.2.3:8001 ${nodes} 1 | tee test/data_et1000/wcp-1-${nodes}-${x}; sleep 3; test/cleanup.sh; done; done
+time for x in $(seq 1 7); do for nodes in $(seq 1 2 35); do time node test/wait_chat_propagate.js 192.168.2.3:8001 ${nodes} 1 | tee ${outdir}/wcp-1-${nodes}-${x}; sleep 3; test/cleanup.sh; done; done
 
 # chat propagate 100 messages:
-time for x in $(seq 1 7); do for nodes in $(seq 1 2 31); do time node test/wait_chat_propagate.js 192.168.2.3:8001 ${nodes} 100 | tee test/data_et1000/wcp-100-${nodes}-${x}; sleep 3; test/cleanup.sh; done; done
+time for x in $(seq 1 7); do for nodes in $(seq 1 2 35); do time node test/wait_chat_propagate.js 192.168.2.3:8001 ${nodes} 100 | tee ${outdir}/wcp-100-${nodes}-${x}; sleep 3; test/cleanup.sh; done; done
 
 # kill 1 node:
-time for x in $(seq 1 7); do for nodes in $(seq 1 2 31); do time node test/wait_kill_nodes.js 192.168.2.3:8001 ${nodes} 1 | tee test/data_et1000/wkn-1-${nodes}-${x}; sleep 3; test/cleanup.sh; done; done
+time for x in $(seq 1 7); do for nodes in $(seq 1 2 35); do time node test/wait_kill_nodes.js 192.168.2.3:8001 ${nodes} 1 | tee ${outdir}/wkn-1-${nodes}-${x}; sleep 3; test/cleanup.sh; done; done
 
-# kill half nodes (162min):
-time for x in $(seq 1 7); do for nodes in $(seq 1 2 31); do time node test/wait_kill_nodes.js 192.168.2.3:8001 ${nodes} | tee test/data_et1000/wkn-half-${nodes}-${x}; sleep 3; test/cleanup.sh; done; done
+# kill half nodes:
+time for x in $(seq 1 7); do for nodes in $(seq 1 2 35); do time node test/wait_kill_nodes.js 192.168.2.3:8001 ${nodes} | tee ${outdir}/wkn-half-${nodes}-${x}; sleep 3; test/cleanup.sh; done; done
